fc6a5b64b2b07850e8c95014f65f80ff83e2f324
42911MjG <milagrosj.gallardo@gmail.com>
2023-03-03 12:07:22 -0300

    Se agrega el Proyecto

--- src/main/java/com/mycompany/proyecto/Proyecto.java
+++ src/main/java/com/mycompany/proyecto/Proyecto.java
@@ -1,7 +1,6 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package com.mycompany.proyecto;
 
 /**
@@ -12,18 +11,23 @@ public class Proyecto {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-   
+
         int unNum = 14;
         while (unNum >= 5) {
             System.out.println(unNum);
-            unNum = unNum -1;
+            unNum = unNum - 1;
         } // Me muestra números dentro del conjunto [5;14]
-        
-        for (int n = 5; n <= 14; n++){
-            if (n % 2 == 0){
+
+        for (int n = 5; n <= 14; n++) {
+            if (n % 2 == 0) {
                 System.out.println(n);
             } // Me muestra números pares en [5;14]
         }
-    //hola
+        int unNumero = 10;
+        while (unNumero > 0) {
+            System.out.println(unNumero);
+            unNumero = unNumero - 1;
+        }
+
     }
 }
