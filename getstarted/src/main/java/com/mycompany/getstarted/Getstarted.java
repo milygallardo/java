767ba60028850183b22f5a2c4532b03e4ade3d48
42911MjG <milagrosj.gallardo@gmail.com>
2023-03-07 22:46:52 -0300

    espero que se hayan enviado los cambios

---
+++ getstarted/src/main/java/com/mycompany/getstarted/Getstarted.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.getstarted;
+
+/**
+ *
+ * @author Diego
+ */
+public class Getstarted {
+
+    public static void main(String[] args) {
+int myNum = 15;
+myNum = 20;  // myNum is now 20
+System.out.println(myNum);
+final int myNumber = 15;
+System.out.println(myNumber);
+    }  /*If you don't want others (or yourself) to overwrite existing values, 
+    use the final keyword,
+    this will declare the variable as "final", which means unchangeable and read-only):*/
+}
